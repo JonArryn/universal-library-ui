56093dfb9e6c8e7bd2fedd2452517ab0fb544038
Jon Arryn <development@arryn.net>
2024-12-28 20:32:46 -0500

    removed console statement and added navigation upon successful creation

--- src/app/pages/Library/CreateLibraryPage.tsx
+++ src/app/pages/Library/CreateLibraryPage.tsx
@@ -7,21 +7,20 @@ import FormSubmitButton from '../../../components/form/FormSubmitButton.tsx';
 import FormError from '../../../components/form/FormError.tsx';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import apiService from '../../../api/apiService.ts';
+import { useNavigate } from 'react-router-dom';
 
 interface ICreateLibrary extends FormInputs {
     name: string;
 }
 
 function CreateLibraryPage() {
+    const navigate = useNavigate();
     const methods = useForm<ICreateLibrary>();
     const handleCreateLibrary: SubmitHandler<ICreateLibrary> = async function (
         libraryFormData
     ) {
-        const createLibraryResponse = await apiService.post(
-            '/library',
-            libraryFormData
-        );
-        console.log(createLibraryResponse);
+        await apiService.post('/library', libraryFormData);
+        navigate('/app/library');
     };
     return (
         <>
